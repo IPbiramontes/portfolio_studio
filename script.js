424d3917b6ac333e50635a12367ff303efd732ee
Isabela Biramontes <ibiramontes.student@hsfi.us>
2019-03-05 02:12:46 +0000

    repushing change

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 /*global $*/
 $(document).ready(function(){
-   console.log("hey");
+   console.log("testing");
+   $("#circuitImage").hover(function(){
+    $(this).css("height", "100%", "width", "100%");
+    }, function(){
+    $(this).css("height", "50%", "width", "auto");
+  });
+  $("#infoClick").click(function(){
+      alert("To access these pages, click it's caption. Thanks for visiting!");
+  });
+   
+    
 });
